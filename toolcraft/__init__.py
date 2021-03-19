83e99fcea96879421533919e3b02bca4a865581a
SpikingNeuron <praveenneuron@gmail.com>
2021-03-19 14:34:04 +0100

    poetry more checks

--- toolcraft/__init__.py
+++ toolcraft/__init__.py
@@ -8,7 +8,6 @@ Resolving __version__
     https://github.com/python-poetry/poetry/pull/2366#issuecomment-652418094
   + Note that bump2version tends to modify the string so always use poetry
 """
-from importlib.metadata import version, PackageNotFoundError
 
 __author__ = """Praveen Kulkarni"""
 __email__ = 'praveenneuron@gmail.com'
@@ -16,6 +15,9 @@ __email__ = 'praveenneuron@gmail.com'
 
 # Note that this is done as code cannot know the version number and it is the job of pyproject.toml
 try:
+    from importlib.metadata import version, PackageNotFoundError
     __version__ = version(__name__)
 except PackageNotFoundError as pnf:
-    __version__ = '[dont know version as locally loaded]'
+    __version__ = 'cannot estimate version'
+except ModuleNotFoundError as mnf:
+    __version__ = 'cannot estimate version'

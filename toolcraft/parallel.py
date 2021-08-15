8a6222a3d3bcf94df1d37f4219226dffa1c62832
SpikingNeuron <praveenneuron@gmail.com>
2021-08-15 23:49:05 +0200

    aa

--- toolcraft/parallel.py
+++ toolcraft/parallel.py
@@ -20,6 +20,8 @@ import time
 import numpy as np
 import abc
 
+from . import util
+
 _KILL_PILL = "__KILL_PILL__"
 
 
@@ -41,16 +43,25 @@ class _Task(Task):
         return f"Task result {self.name} {id(task_runner)}"
 
 
-class TaskRunner(mp.Process):
+class TaskRunner:
+    ...
 
-    # noinspection PyAttributeOutsideInit
-    def setup_queues(
+
+class ProcessWithQueues(mp.Process):
+
+    def __init__(
         self,
         task_queue: mp.JoinableQueue,
         result_queue: mp.Queue,
+        task_runner: TaskRunner
     ):
+        # call super
+        super().__init__()
+
+        # setup queues
         self.task_queue = task_queue
         self.result_queue = result_queue
+        self.task_runner = task_runner
 
     def run(self):
         while True:
@@ -60,7 +71,7 @@ class TaskRunner(mp.Process):
                 self.task_queue.task_done()
                 break
             _result = next_task.do_it(
-                self
+                self.task_runner
             )
             self.task_queue.task_done()
             self.result_queue.put(_result)
@@ -68,37 +79,76 @@ class TaskRunner(mp.Process):
 
 class TaskRunnerPool:
 
+    @property
+    @util.CacheResult
+    def processes(self) -> t.List[mp.Process]:
+        return [
+            ProcessWithQueues(
+                task_queue=self.task_queue, result_queue=self.result_queue,
+                task_runner=_tr
+            ) for _tr in self.task_runners
+        ]
+
     def __init__(
         self,
-        tasks: t.List[Task],
+        tasks: t.Iterator[Task],
         task_runners: t.List[TaskRunner],
     ):
-        self.tasks = tasks
         self.task_runners = task_runners
         self.task_queue = mp.JoinableQueue()
         self.result_queue = mp.Queue()
 
+        def _task_yielder() -> t.Iterator[Task]:
+            # add tasks
+            for _ in tasks:
+                yield _
+
+            # add kill pill task for each task runner
+            for _ in range(len(task_runners)):
+                yield _KILL_PILL
+
+        self.task_iterator = _task_yielder()
+
+    def __enter__(self):
+        self.start()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop()
+
     def start(self):
         # start task runners
-        for _tr in self.task_runners:
-            _tr.setup_queues(
-                task_queue=self.task_queue, result_queue=self.result_queue
-            )
-            _tr.start()
+        for _p in self.processes:
+            _p.start()
 
-        # add tasks
-        for _t in self.tasks:
-            self.task_queue.put(_t)
+        # at start we will put some tasks
+        for _ in range(3*len(self.task_runners)):
+            self.task_queue.put(next(self.task_iterator))
 
-        # add kill pill task for each task runner
-        for _ in range(len(self.task_runners)):
-            self.task_queue.put(_KILL_PILL)
+    def stop(self):
+        # start task runners
+        for _p in self.processes:
+            _p.terminate()
 
     def get_results(self) -> t.Iterable[t.Any]:
         _time_for_retrieval = 2.
+        _num_task_runners = len(self.task_runners)
+        _tasks_available = True
+
         # yield until queue is empty
         while True:
+            # go to sleep
             time.sleep(_time_for_retrieval)
+
+            # grow task queue if small
+            if _tasks_available:
+                if self.task_queue.qsize() < 2*_num_task_runners:
+                    for _ in range(_num_task_runners):
+                        try:
+                            self.task_queue.put(next(self.task_iterator))
+                        except StopIteration:
+                            _tasks_available = False
+                            break
+
             try:
                 # estimate time to retrieve next result based on queue length
                 _num_results = self.result_queue.qsize()
